7eb4166a7087d671e8c2e0047a68378623c51edf
jihoon <akdlsz21@gmail.com>
2021-10-03 02:37:36 +0900

    init

---
+++ src/components/Charts/index.js
@@ -0,0 +1,8 @@
+import ExampleChart from './ExampleChart';
+import Column3D from './Column3D';
+import Bar3D from './Bar3D';
+import Pie3D from './Pie2D';
+import Doughnut2D from './Doughnut2d';
+import Pie2D from './Pie2D';
+
+export { ExampleChart, Pie2D, Pie3D, Column3D, Bar3D, Doughnut2D };
---
+++ src/components/Repos.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GithubContext } from '../context/context';
+import { ExampleChart, Pie2D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
+const Repos = () => {
+	const { repos } = React.useContext(GithubContext);
+	const languages = repos.reduce((total, item) => {
+		const { language, stargazers_count } = item;
+		if (!language) return total;
+		if (!total[language]) {
+			total[language] = {
+				label: language,
+				value: 1,
+				stars: stargazers_count,
+			};
+		} else {
+			total[language] = {
+				...total[language],
+				value: total[language].value + 1,
+				stars: total[language].stars + stargazers_count,
+			};
+		}
+		return total;
+	}, {});
+	console.log(languages);
+	const mostUsed = Object.values(languages)
+		.sort((a, b) => {
+			return b.value - a.value;
+		})
+		.slice(0, 5);
+
+	// most stars per language
+	const mostPopular = Object.values(languages)
+		.sort((a, b) => {
+			return b.stars - a.stars;
+		})
+		.map((item) => {
+			return { ...item, value: item.stars };
+		})
+		.slice(0, 5);
+	console.log(mostPopular);
+
+	const chartData = [
+		{
+			label: 'HTML',
+			value: '25',
+		},
+		{
+			label: 'CSS',
+			value: '32',
+		},
+		{
+			label: 'Javascript',
+			value: '80',
+		},
+	];
+
+	return (
+		<section className="section">
+			<Wrapper className="section-center">
+				<Pie2D data={mostUsed} />
+				<Doughnut2D data={mostPopular} />
+				{/* <ExampleChart data={chartData}></ExampleChart>; */}
+			</Wrapper>
+		</section>
+	);
+};
+
+const Wrapper = styled.div`
+	display: grid;
+	justify-items: center;
+	gap: 2rem;
+	@media (min-width: 800px) {
+		grid-template-columns: 1fr 1fr;
+	}
+
+	@media (min-width: 1200px) {
+		grid-template-columns: 2fr 3fr;
+	}
+	
+		'' 
+	div {
+		width: 100% !important;
+	}
+	.fusioncharts-container {
+		width: 100% !important;
+	}
+	svg {
+		width: 100% !important;
+		border-radius: var(--radius) !important;
+	} 
+	}
+`;
+
+export default Repos;
